f0f11a161325b530c602c86eace93de65f14b595
Mahmoud Nasser <mahmoud.naser@appssquare.com>
2022-06-26 11:17:45 +0200

    Initial Commit

---
+++ LeanScaleCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeanScaleCaseStudy
+//
+//  Created by Mahmoud Nasser on 26/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
